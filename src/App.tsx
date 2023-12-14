73d0a1f8eadcf91e2f891cc80dc49eb2451d8dd0
HassanKhan123 <hk2602809@gmail.com>
2023-12-14 22:18:50 +0100

    added: routes and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import SigninForm from "./_auth/forms/SigninForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 import "./globals.css";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
